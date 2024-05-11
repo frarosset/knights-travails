90f4995788823ab9ef910aaf4ffaacce985551d9
frarosset <37275581+frarosset@users.noreply.github.com>
2024-05-11 20:57:10 +0200

    Add addNeighbor() and print() methods to Node class

--- Node.js
+++ Node.js
@@ -1,7 +1,25 @@
 export default class Node{
+    #id;
     #neighbors = [];
+
+    constructor(id){
+        this.#id = id;
+    }
+
+    get id(){
+        return this.#id;
+    }
 
     get neighbors(){
         return this.#neighbors;
+    }
+
+    addNeighbor(nodeId){
+        this.#neighbors.push(nodeId);
+    }
+
+
+    print(){
+        console.log(`[${this.#id}] -> {${this.#neighbors.map(itm => '['+itm+']')}}`);
     }
 }
---
+++ testNode.js
@@ -0,0 +1,23 @@
+
+import Node from './Node.js'
+
+console.log('Generic node of a graph, identified by a number');
+let nodeId = 3;
+let neighborsId = [1,5,6,2];
+
+let node = new Node(nodeId);
+node.print();
+
+neighborsId.forEach(neighborId => node.addNeighbor(neighborId));
+node.print();
+
+// Nodes of a 2d grid graph, identified by [row,column]
+console.log('\nGeneric node of a 2d grid graph, identified by [row,column]');
+nodeId = [3,4];
+neighborsId = [[2,4],[5,6],[4,6],[7,9]];
+
+node = new Node(nodeId);
+node.print();
+
+neighborsId.forEach(neighborId => node.addNeighbor(neighborId));
+node.print();
